5e86b54cdbf32c9b25435595fa4b6c71b369b01c
varsh <varsh@Varsha>
2023-12-05 15:22:21 +0530

    first eclipse

---
+++ src/main/java/eclipse1/aaa/App.java
@@ -0,0 +1,13 @@
+package eclipse1.aaa;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
